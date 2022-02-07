4765e1ffcd9e38f25897b61bbf00c268fe14a66a
Ricarda W <yumenoki@gmx.de>
2022-02-07 18:37:31 +0100

    LandMarkDetection 1
    Die Kapsel ist erstmal wieder aus und das Webcambild an, um das Tracking des Mundes zu visualisieren. Stürzt durch Zeile 75/76 ab, auskommentiert geht (aber ohne Mund)

--- Assets/Ricarda/FaceDetect.cs
+++ Assets/Ricarda/FaceDetect.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using OpenCvSharp;
+using OpenCvSharp.Demo;
 using UnityEngine.Diagnostics;
 using UnityEngine.UI;
+using Rect = OpenCvSharp.Rect;
 
 public class FaceDetect : MonoBehaviour
 {
@@ -12,8 +14,15 @@ public class FaceDetect : MonoBehaviour
     private CascadeClassifier cascade; //der ObjectDetector
     private OpenCvSharp.Rect myFace;
 
+    private Point mouth;
+
     public Vector2 faceLocation;
 
+    public TextAsset faceCascade;
+    public TextAsset eyes;
+    public TextAsset shapes;
+    //private FaceProcessorLive<WebCamTexture> landmarks; 
+    private ShapePredictor predictor;
     private void Start()
     {
         //Webcambild in Webcam Textur speichern
@@ -23,7 +32,15 @@ public class FaceDetect : MonoBehaviour
 
         //FaceDetection xml  
         cascade = new CascadeClassifier(Application.dataPath + @"/haarcascade_frontalface_default.xml");
+        
+        byte[] shapeDat = shapes.bytes;
 
+        //landmarks = new FaceProcessorLive<WebCamTexture>();
+        //landmarks.Initialize(faceCascade.text, eyes.text, shapes.bytes);
+        
+        predictor = new ShapePredictor();
+        predictor.LoadData(shapeDat);
+        
     }
 
     void Update()
@@ -35,19 +52,28 @@ public class FaceDetect : MonoBehaviour
         //aus Webcam Textur OpenCV-Material machen, um current frame zum auswerten zu speichern
         Mat frame = OpenCvSharp.Unity.TextureToMat(_webCamTexture);
         findNewFace(frame);
-        //display(frame);
+        display(frame);
     }
 
     void findNewFace(Mat frame)
     {
         var faces = cascade.DetectMultiScale(frame, 1.1, 2, HaarDetectionType.ScaleImage);
-
+        
         if (faces.Length == 1)
         {
             faceLocation = new Vector2(faces[0].Location.X, faces[0].Location.Y);
-            Debug.Log(faces[0].Location);
+            
             myFace = faces[0]; //Location und Size des ersten Gesichts 
+            findLandmarks(frame, myFace);
         }
+    }
+
+    void findLandmarks(Mat frame, Rect face)
+    {
+        //landmarks.MarkDetected();
+        
+        mouth.X = predictor.DetectLandmarks(frame, myFace)[63].X;
+        mouth.Y = predictor.DetectLandmarks(frame, myFace)[63].Y;
     }
 
     void display(Mat frame)
@@ -55,6 +81,7 @@ public class FaceDetect : MonoBehaviour
         if (myFace != null)
         {
             frame.Circle(myFace.Center, 90, Scalar.Red, 2);
+            frame.Circle(mouth, 20, Scalar.Pink, 2);
         }
         //Webcam Bild mit neuem Kreis auf UI Bild abbilden (Open CV Material muss dafür wieder Textur werden)
         Texture frameToTexture = OpenCvSharp.Unity.MatToTexture(frame);
